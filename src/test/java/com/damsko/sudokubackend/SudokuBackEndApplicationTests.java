8977448fda65f1d629032aec2e11af7448f992ad
Merry Damsko <m.damsko@jcourant.nl>
2024-01-12 20:15:02 +0100

    copy of sudoku project

---
+++ src/test/java/com/damsko/sudokubackend/SudokuBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damsko.sudokubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
