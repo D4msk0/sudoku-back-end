f0946d4f1f8ad6d55007dfc7c552546e6313423a
Merry Damsko <m.d4msk0@gmail.com>
2024-02-06 15:26:13 +0100

    added tests for model with cody.

--- src/main/java/com/damsko/sudokubackend/controller/SudokuController.java
+++ src/main/java/com/damsko/sudokubackend/controller/SudokuController.java
@@ -15,15 +15,15 @@ public class SudokuController {
         //Should I minimize logic here? Maybe init the grid somewhere else?
         //set initial grid
         int[][] initialGrid = {
-                {5, 3, 0, 0, 7, 0, 0, 0, 0},
-                {6, 0, 0, 1, 9, 5, 0, 0, 0},
-                {0, 9, 8, 0, 0, 0, 0, 6, 0},
-                {8, 0, 0, 0, 6, 0, 0, 0, 3},
-                {4, 0, 0, 8, 0, 3, 0, 0, 1},
-                {7, 0, 0, 0, 2, 0, 0, 0, 6},
-                {0, 6, 0, 0, 0, 0, 2, 8, 0},
-                {0, 0, 0, 4, 1, 9, 0, 0, 5},
-                {0, 0, 0, 0, 8, 0, 0, 7, 9}
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 0, 2, 0, 0, 0, 0, 0, 0}
         };
         sudokuModel.setGrid(initialGrid);
         // Add the Sudokumodel to the model to make it available in the view
